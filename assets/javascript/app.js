90e5898e9d5bfa9ace5e99ec00d15bfef28fbd5b
zmulji <znmulji1@gmail.com>
2019-07-07 19:16:59 -0400

    added new questions, fixed question generation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,37 @@
 var q1 = {
-    question: "Who won the 2019 Woman's World Cup Final?",
+    question: "Which country won the 2019 Woman's World Cup?",
     a: "Netherlands",
     b: "USA",
     c: "France",
     d: "Argentina",
     correct: "b"
 }
+var q2 = {
+    question: "Which player was dubbed the 'US Secretary of Defense' during the 2014 Men's World Cup?",
+    a: "Guillermo Ochoa",
+    b: "Manuel Neuer",
+    c: "Tim Howard",
+    d: "Vincent Enyeama",
+    correct: "c"
+}
+var q3 = {
+    question: "Which country placed 3rd at their first appearance at the 1998 Men's World Cup?",
+    a: "Croatia",
+    b: "Uruguay",
+    c: "South Korea",
+    d: "Jamaica",
+    correct: "a"
+}
+var q4 = {
+    question: "What was the score of the France v. Croatia 2018 Men's World Cup Final?",
+    a: "3-3",
+    b: "4-2",
+    c: "2-4",
+    d: "1-0",
+    correct: "b"
+}
+
+var questions = [q1,q2,q3,q4]
 
 var correct = 0
 var incorrect = 0
@@ -29,16 +55,42 @@ each question-answer is an obj
 var qTimer;
 var time = 30;
 var choiceLock = false;
+var newQues;
+var q;
+var qList;
 
 window.onload = function(){
     $(".game-content").hide()   //hide q&a html
+    $(".result").hide()
     
-    function load(q){
-        $(".question").text(q.question)    // load questions & answers into html
-        $(".choice-a").text(q.a)
-        $(".choice-b").text(q.b)
-        $(".choice-c").text(q.c)
-        $(".choice-d").text(q.d)
+
+    function load(){
+        if(qList.length > 0){
+            console.log("load correctly")
+            // q = qList[Math.floor(Math.random() * qList.length)]   
+            q = qList[qList.length - 1]        // pick question from temp list
+            qList.pop(q)                       // remove question from temp list after use
+            $(".question").text(q.question)    // load questions & answers into html
+            $(".choice-a").text(q.a)            
+            $(".choice-b").text(q.b)
+            $(".choice-c").text(q.c)
+            $(".choice-d").text(q.d)
+            $("li").css("color","black")
+            time = 30                           // reset timer
+            $(".time-left").text(timeConverter(time))
+            clearInterval(qTimer)
+            qTimer = setInterval(count, 500)
+            choiceLock = false                  // unlock answer select
+        }
+        else {
+            console.log("load wrong")
+            $(".game-content").hide()
+            $(".result").show()
+            $(".question").text("Results")
+            $(".correct").text(correct)
+            $(".incorrect").text(incorrect)
+            $(".unanswered").text(unanswered)
+        }
     }
     function timeConverter(t) {
         //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
@@ -71,12 +123,14 @@ window.onload = function(){
     
     $(".start-btn").click(function(){
         $(".start-btn").hide()
-        $(".game-content").show()   
-        
-        load(q1)
-        $(".time-left").text(timeConverter(time))
-        qTimer = setInterval(count, 1000)
-    
+        $(".result").hide()
+        $(".game-content").show()  
+        qList = questions                   // create temp list of questions 
+        console.log(qList.length)
+        load()
+        // newQues = setTimeout(load, 15000)
+
+
     })
 
     $("li").click(function(){
@@ -84,21 +138,32 @@ window.onload = function(){
             choiceLock = true
             clearInterval(qTimer)
             var select = $(this).text()
-            if(check(q1,select)){
-                $(this).css("background", "rgb(0, 142, 106)").css("color", "rgb(0, 174, 130)")
+            if(check(q,select)){
+                $(this).css("color", "rgb(0, 174, 130)")
+                console.log("correct")
+                var correctChoice = ".li-" + q.correct
+                $(correctChoice).fadeToggle(1000).fadeToggle(1000).fadeToggle(1000).fadeToggle(1000)
                 correct++
             }
             else{
-                $(this).css("background", "rgb(255, 26, 75)").css("color","rgb(255, 87, 112)")
-                var correctChoice = ".li-"+q1.correct
-                console.log(correctChoice)
-                $(correctChoice).fadeToggle(1000).fadeToggle(1000).fadeToggle(1000).fadeToggle(1000).fadeToggle(1000).fadeToggle(1000)
+                $(this).css("color","rgb(255, 87, 112)")
+                console.log("incorrect")
+                var correctChoice = ".li-"+q.correct
+                $(correctChoice).fadeToggle(1000).fadeToggle(1000).fadeToggle(1000).fadeToggle(1000)
                 incorrect++
             }
-            
+            newQues = setTimeout(load, 4100)   
         }
+    })
 
-
+    $(".restart-btn").click(function () {
+        console.log("restart worked")
+        $(".start-btn").hide()
+        $(".result").hide()
+        $(".game-content").show()
+        qList = questions                   // create temp list of questions 
+        console.log(qList.length)
+        load()
     })
     
 
